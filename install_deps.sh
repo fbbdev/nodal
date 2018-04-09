0e6c47b8c0d50637323301f381ba5ac81d87e05a
Fabio Massaioli <fabio.massaioli@gmail.com>
2018-04-09 12:39:03 +0200

    Add clang to travis build

--- install_deps.sh
+++ install_deps.sh
@@ -3,13 +3,6 @@ set -e
 
 mkdir -p "$HOME/deps"
 
-if [ ! -d "$HOME/deps/cmake/bin" ]; then
-    wget --no-check-certificate -O cmake.tar.gz "https://cmake.org/files/v3.9/cmake-3.9.0-Linux-x86_64.tar.gz"
-    tar -xf cmake.tar.gz
-    mv cmake-* "$HOME/deps/cmake"
-    rm -rf cmake.tar.gz cmake-*
-fi
-
 if [ ! -d "$HOME/deps/boost/include/boost" ]; then
     wget -O boost.tar.bz2 "https://sourceforge.net/projects/boost/files/boost/1.56.0/boost_1_56_0.tar.bz2/download"
     tar -xf boost.tar.bz2
